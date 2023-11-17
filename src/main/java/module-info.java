7fef084ebb6005a344c3b04b0a374bba69f6688a
altynaykemelova <kemelovaaltynay24@gmail.com>
2023-11-17 09:00:11 +0600

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.fitnespro {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.fitnespro to javafx.fxml;
+    exports com.example.fitnespro;
+}
